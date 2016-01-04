d3569b15d813683f16cb57f5f694c907b34f7e45
Lionel Félicité <deogracia@free.fr>
2016-01-05 00:23:42 +0100

    Check that all step option are given something (no check for now)

--- run.sh
+++ run.sh
@@ -1,1 +1,40 @@
-success "First try of this step"
+#!/bin/sh
+set -e
+
+
+if [ -z "$WERCKER_SCP_DEPLOY_HOST" ]
+then
+    fail 'missing host option, please add this the scp-deploy step in wercker.yml'
+fi
+
+if [ -z "$WERCKER_SCP_DEPLOY_SSHPRIVATEKEY" ]
+then
+    fail 'missing sshprivatekey option, please add this the scp-deploy step in wercker.yml'
+fi
+
+if [ -z "$WERCKER_SCP_DEPLOY_USER" ]
+then
+    fail 'missing user option, please add this the scp-deploy step in wercker.yml'
+fi
+
+if [ -z "$WERCKER_SCP_DEPLOY_DESTINATION" ]
+then
+    fail 'missing destiation option, please add this the scp-deploy step in wercker.yml'
+fi
+
+if [ -z "$WERCKER_SCP_DEPLOY_HIDE_SSH_CNX_INFO_FROM_LOG" ]
+then
+    fail 'missing hide-ssh-cnx-info-from-log option, please add this the scp-deploy step in wercker.yml'
+fi
+
+if [ -z "$WERCKER_SCP_DEPLOY_SSHPORT" ]
+then
+    fail 'missing sshport option, please add this the scp-deploy step in wercker.yml or something went wrong with the defaults'
+fi
+
+if [ -z "$WERCKER_SCP_DEPLOY_SOURCE" ]
+then
+    fail 'missing source option, please add this the scp-deploy step in wercker.yml or something went wrong with the defaults'
+fi
+
+success "deploy ok :D"
